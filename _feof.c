f821ef97be4ca25bc21e6bed70f5c9be6805deac
JordanHolberton <jordan.chatillon76@laposte.net>
2024-08-19 11:13:51 +0200

    fix betty codding style in _feof

--- _feof.c
+++ _feof.c
@@ -6,20 +6,21 @@
  *
  * Return: 1 if the end-of-file indicator is set for the stream, 0 otherwise.
  */
-int _feof(FILE *stream) {
-    int c = fgetc(stream);
+int _feof(FILE *stream)
+{
+	int c = fgetc(stream);
 
-    if (c == EOF) {
-        if (feof(stream))
-	{
-            return (1);
-        } 
-	else
-	{
-            return (0);
-        }
-    }
+	if (c == EOF) {
+		if (feof(stream))
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
 
-    ungetc(c, stream);
-    return (0);
+	ungetc(c, stream);
+	return (0);
 }
