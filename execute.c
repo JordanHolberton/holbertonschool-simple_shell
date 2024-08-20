42b965c07f62a293f2503a10a44f76b6b67ab262
JordanHolberton <jordan.chatillon76@laposte.net>
2024-08-20 10:45:34 +0200

    Fix betty codding style

--- execute.c
+++ execute.c
@@ -8,42 +8,42 @@
  */
 int execute(char **args)
 {
-    pid_t pid;
-    int status;
-    char *command_path;
+	pid_t pid;
+	int status;
+	char *command_path;
 
-    if (args[0] == NULL)
-        return (1);
+	if (args[0] == NULL)
+		return (1);
 
-    if (strcmp(args[0], "exit") == 0)
-        return (0);
+	if (strcmp(args[0], "exit") == 0)
+		return (0);
 
-    command_path = find_command_path(args[0]);
-    if (command_path == NULL)
-    {
-        fprintf(stderr, "hsh: %s: command not found\n", args[0]);
-        return (1);
-    }
+	command_path = find_command_path(args[0]);
+	if (command_path == NULL)
+	{
+		fprintf(stderr, "hsh: %s: command not found\n", args[0]);
+		return (1);
+	}
 
-    pid = fork();
-    if (pid == 0)
-    {
-        if (execve(command_path, args, environ) == -1)
-            perror("hsh");
-        _exit(EXIT_FAILURE);
-    }
-    else if (pid < 0)
-    {
-        perror("hsh");
-    }
-    else
-    {
-        do {
-            waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execve(command_path, args, environ) == -1)
+			perror("hsh");
+		_exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+	{
+		perror("hsh");
+	}
+	else
+	{
+		do {
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
 
-    free(command_path);
-    return (1);
+	free(command_path);
+	return (1);
 }
 
--- find_command_path.c
+++ find_command_path.c
@@ -8,39 +8,39 @@
  */
 char *find_command_path(char *cmd)
 {
-    char *path, *path_copy, *dir;
-    char *full_path;
-    int len;
+	char *path, *path_copy, *dir;
+	char *full_path;
+	int len;
 
-    path = getenv("PATH");
-    if (path == NULL)
-        return (NULL);
+	path = getenv("PATH");
+	if (path == NULL)
+		return (NULL);
 
-    path_copy = strdup(path);
-    if (path_copy == NULL)
-        return (NULL);
+	path_copy = strdup(path);
+	if (path_copy == NULL)
+		return (NULL);
 
-    full_path = malloc(1024);
-    if (full_path == NULL)
-    {
-        free(path_copy);
-        return (NULL);
-    }
+	full_path = malloc(1024);
+	if (full_path == NULL)
+	{
+		free(path_copy);
+		return (NULL);
+	}
 
-    dir = strtok(path_copy, ":");
-    while (dir)
-    {
-        len = snprintf(full_path, 1024, "%s/%s", dir, cmd);
-        if (len >= 0 && access(full_path, X_OK) == 0)
-        {
-            free(path_copy);
-            return (full_path);
-        }
-        dir = strtok(NULL, ":");
-    }
+	dir = strtok(path_copy, ":");
+	while (dir)
+	{
+		len = snprintf(full_path, 1024, "%s/%s", dir, cmd);
+		if (len >= 0 && access(full_path, X_OK) == 0)
+		{
+			free(path_copy);
+			return (full_path);
+		}
+		dir = strtok(NULL, ":");
+	}
 
-    free(full_path);
-    free(path_copy);
-    return (NULL);
+	free(full_path);
+	free(path_copy);
+	return (NULL);
 }
 
